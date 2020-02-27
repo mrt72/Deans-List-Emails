1cb7b262531b7f214e56ff3fee13da59efef1832
Matt Tobin <60553988+mrt72@users.noreply.github.com>
2020-02-27 13:43:01 -0500

    Add files via upload

--- FindPeople.py
+++ FindPeople.py
@@ -1,22 +1,22 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException as nSE
 import csv
 
 driver = webdriver.Firefox()
 driver.get("https://find.pitt.edu")
 
-# f = open("deans_list_fall_2019.csv")
-# reader = csv.reader(f)
-# names = []
-# for row in reader:
-#   names.append(row)
-
-
-f2 = open("test.csv")
-reader = csv.reader(f2)
+f = open("deans_list_fall_2019.csv")
+reader = csv.reader(f)
 names = []
 for row in reader:
     names.append(row)
+
+# f2 = open("test.csv")
+# reader = csv.reader(f2)
+# names = []
+# for row in reader:
+# names.append(row)
 
 
 for i in names:
@@ -25,11 +25,22 @@ for i in names:
     search.clear()  # Clears existing text in the search bar
     search.send_keys(i)  # Types what you want to search
     search.send_keys(Keys.RETURN)  # Return key in order to search
-    driver.implicitly_wait(3)  # Waits 3 seconds for results
-    email = driver.find_element_by_partial_link_text('pitt')  # Finds location of Pitt email address in <a> tag
-    dl_email = email.get_attribute('href')
-    dl_email = dl_email.replace("mailto:", "")
-    f1.write("Name: " + str(i) + "    Email: " + dl_email + "\n")  # gets the plaintext of email, writes to emails.txt
-    f1.close()  # Closes emails.txt
-    assert "No results found" not in driver.page_source
-    driver.refresh()
+    driver.implicitly_wait(10)  # Waits 4 seconds for results
+    try:
+        email = driver.find_element_by_partial_link_text('@').text  # Finds location of Pitt email address in <a> tag
+        results = driver.find_element_by_id("resultsInfo").text
+        results = results[0]
+        if int(results) == 1:
+            f1.write("Name: " + str(i) + "    Email: " + email + "\n")  # gets the plaintext of email, writes to emails.txt
+            # email = "Find Manually"
+        elif int(results) > 1:
+            f1.write("Name: " + str(i) + "    Email: " + email + " (Verify, Duplicate Names)\n")
+        elif int(results) == 0:
+            f1.write("Name: " + str(i) + "    Email: Not found!" + "\n")
+        assert "0 results found" not in driver.page_source
+        # dl_email = email.get_attribute('href') (Not needed, can just use .text at end of email find_element)
+        # dl_email = dl_email.replace("mailto:", "")
+        f1.close()  # Closes emails.txt
+        driver.refresh()
+    except nSE:  # exception to catch
+        f1.write("Name: " + str(i) + "     Email: person not found \n")
